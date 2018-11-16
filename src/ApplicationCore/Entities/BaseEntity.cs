7a0e4a525744a7da944f1ba8d0aee823e9a85e73
Vladimir Filipovic <vladofilipovic@hotmail.com>
2018-11-16 13:28:49 +0100

    proemnio i dodao komentare

--- src/ApplicationCore/Entities/BaseEntity.cs
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -1,9 +1,16 @@
 ﻿namespace Microsoft.eShopWeb.ApplicationCore.Entities
 {
     // This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
-    // Using non-generic integer types for simplicity and to ease caching logic
+    // Using non-generic integer types for simplicity and to ease caching logic             
+    /// <summary>
+    /// Ovo osnovna klasa za entitete, iy koje se izvode svi ostali entiteti
+    /// </summary>
     public class BaseEntity
     {
+        /// <summary>
+        /// Osobina koja vra;e entitete
+        /// </summary>
+        /// <value> vrednost identifikatora</value>
         public int Id { get; set; }
     }
 }
